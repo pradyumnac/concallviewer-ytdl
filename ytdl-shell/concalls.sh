4c113b70707ab6a63e8ee0557c2377a80b3b328e
pradyumna <pady92@gmail.com>
2021-02-13 08:34:24 +0530

    concalls linux script

--- ytdl-shell/concalls.sh
+++ ytdl-shell/concalls.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-yt-dl -ciw -a ~/repos/concallviewer/ytdl-shell/pl_concall.txt -o "/home/pi/files/Downloads/ytdl/%(uploader)s/%(title)s -  %(upload_date)s.%(ext)s" --format=bestaudio[ext=m4a] --write-thumbnail --write-info-json --write-automatic-sub
+yt-dl -ciw -a ~/repos/concallviewer/ytdl-shell/pl_concall.txt -o "/home/pi/files/Downloads/ytdl/%(uploader)s/%(title)s -  %(upload_date)s.%(ext)s" --format=bestaudio[ext=m4a] --write-thumbnail --write-info-json --write-automatic-sub --playlist-end 295 --playlist-start 1
+# last 5p are non earling call videos
